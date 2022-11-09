fc2b07f13fc805cd01105be25d3e5f39adb3e30e
riku1227 <17927874+riku1227@users.noreply.github.com>
2022-11-09 21:42:35 +0900

    :bookmark: v1.2.0

--- lib/novelai_manager.dart
+++ lib/novelai_manager.dart
@@ -1,9 +1,9 @@
 class NovelAIManager {
   // ソフトのバージョン
-  static const version = "1.1.1";
+  static const version = "1.2.0";
   // ソフトのバージョンコード
   // アップデート確認時の比較に使われる
-  static const versionCode = 4;
+  static const versionCode = 5;
 
   //データーベースを保存するフォルダ
   static const baseDBFolderName = "gallery_db";
